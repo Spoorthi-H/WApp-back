fe9e81f3301e44beaebf37028d1c089f578ee2e2
Spoorthi-H <spoorthihm51@gmail.com>
2023-05-09 11:12:49 +0530

    b1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors=require('cors')
 const app = express();
 app.use(bodyparser.urlencoded({extended:true}))
 app.use(bodyparser.json())
-//app.use(cors({origin: "https://localhost:5000"}));
+app.use(cors({origin: ["https://localhost:5000","https://weather-app-api-1aiy.onrender.com"]}));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
